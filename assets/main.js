6537346a9f8f967ef503e6945ae55bcd2a54931b
yevheniia <jene4ka1818@gmail.com>
2019-03-20 10:12:09 -0400

    fin

--- assets/main.js
+++ assets/main.js
@@ -11,6 +11,8 @@ var config = {
   var database = firebase.database();
   var currentTime = moment().format();
 
+  
+
   $('#submitBtnText').on('click', function(event){
       event.preventDefault();
       if ($('#formGroupExampleInput').val() === "" ||
@@ -70,13 +72,17 @@ var config = {
     function onSignIn(googleUser) {
       // Useful data for your client-side scripts:
       var profile = googleUser.getBasicProfile();
+      $('.card').css('display', 'block');
+
+
+      //==============================console===================================================================//
       console.log("ID: " + profile.getId()); // Don't send this directly to your server!
       console.log('Full Name: ' + profile.getName());
       console.log('Given Name: ' + profile.getGivenName());
       console.log('Family Name: ' + profile.getFamilyName());
       console.log("Image URL: " + profile.getImageUrl());
       console.log("Email: " + profile.getEmail());
-
+     //=========================================================================================================//
       // The ID token you need to pass to your backend:
       var id_token = googleUser.getAuthResponse().id_token;
       console.log("ID Token: " + id_token);
